c9019596084c311670bb1e5c7253e04337d8140e
yashwanthpamireddy <115986432+yashwanthpamireddy@users.noreply.github.com>
2024-03-11 16:22:26 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "This is Yashwanth\n";
+  cout << "webhook built successfully\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
